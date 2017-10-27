9f70b0cd665f708b73c5471032bb030069f85834
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-27 03:23:17 +0300

    Added environment names contants

---
+++ CommandLine/EnvironmentName.cs
@@ -0,0 +1,9 @@
+﻿namespace CommandLine
+{
+    public static class EnvironmentName
+    {
+        public static readonly string Development = "Development";
+        public static readonly string Staging = "Staging";
+        public static readonly string Production = "Production";
+    }
+}
