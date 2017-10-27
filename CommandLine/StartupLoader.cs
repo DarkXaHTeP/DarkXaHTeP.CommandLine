58f8d74a103aab4dd862b05709026a5b3af20e67
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-28 00:47:50 +0300

    Added StartupLoader and methods

---
+++ CommandLine/StartupLoader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandLine
+{
+    public static class StartupLoader
+    {
+        public static StartupMethods LoadMethods(IServiceProvider hostingServiceProvider, Type startupType,
+            string environmentName)
+        {
+            var configureMethod = FindConfigureDelegate(startupType, environmentName);
+
+            var servicesMethod = FindConfigureServicesDelegate(startupType, environmentName);
+
+            object instance = null;
+            if (!configureMethod.MethodInfo.IsStatic || (servicesMethod != null && !servicesMethod.MethodInfo.IsStatic))
+            {
+                instance = ActivatorUtilities.GetServiceOrCreateInstance(hostingServiceProvider, startupType);
+            }
+
+            return new StartupMethods(instance, configureMethod.Build(instance), servicesMethod?.Build(instance));
+        }
+
+        private static ConfigureBuilder FindConfigureDelegate(Type startupType, string environmentName)
+        {
+            var configureMethod = FindMethod(startupType, "Configure{0}", environmentName, required: true);
+            return new ConfigureBuilder(configureMethod);
+        }
+        
+        private static ConfigureServicesBuilder FindConfigureServicesDelegate(Type startupType, string environmentName)
+        {
+            var servicesMethod = FindMethod(startupType, "Configure{0}Services", environmentName, required: false);
+            return new ConfigureServicesBuilder(servicesMethod);
+        }
+        
+        private static MethodInfo FindMethod(Type startupType, string methodName, string environmentName, bool required = true)
+        {
+            var methodNameWithEnv = string.Format(CultureInfo.InvariantCulture, methodName, environmentName);
+            var methodNameWithNoEnv = string.Format(CultureInfo.InvariantCulture, methodName, "");
+
+            var methods = startupType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            var selectedMethods = methods.Where(method => method.Name.Equals(methodNameWithEnv, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (selectedMethods.Count > 1)
+            {
+                throw new InvalidOperationException(string.Format("Having multiple overloads of method '{0}' is not supported.", methodNameWithEnv));
+            }
+            if (selectedMethods.Count == 0)
+            {
+                selectedMethods = methods.Where(method => method.Name.Equals(methodNameWithNoEnv, StringComparison.OrdinalIgnoreCase)).ToList();
+                if (selectedMethods.Count > 1)
+                {
+                    throw new InvalidOperationException(string.Format("Having multiple overloads of method '{0}' is not supported.", methodNameWithNoEnv));
+                }
+            }
+
+            var methodInfo = selectedMethods.FirstOrDefault();
+            if (methodInfo == null)
+            {
+                if (required)
+                {
+                    throw new InvalidOperationException(string.Format("A public method named '{0}' or '{1}' could not be found in the '{2}' type.",
+                        methodNameWithEnv,
+                        methodNameWithNoEnv,
+                        startupType.FullName));
+
+                }
+                return null;
+            }
+            if (methodInfo.ReturnType != typeof(void))
+            {
+                if (required)
+                {
+                    throw new InvalidOperationException(string.Format("The '{0}' method in the type '{1}' must have a return type of 'void'.",
+                        methodInfo.Name,
+                        startupType.FullName));
+                }
+                return null;
+            }
+            return methodInfo;
+        }
+    }
+}
---
+++ CommandLine/StartupMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandLine
+{
+    public class StartupMethods
+    {
+        public StartupMethods(object instance, Action<IApplicationBuilder> configure, Action<IServiceCollection> configureServices)
+        {
+            Debug.Assert(configure != null);
+            Debug.Assert(configureServices != null);
+
+            StartupInstance = instance;
+            ConfigureDelegate = configure;
+            ConfigureServicesDelegate = configureServices;
+        }
+
+        public object StartupInstance { get; }
+        public Action<IServiceCollection> ConfigureServicesDelegate { get; }
+        public Action<IApplicationBuilder> ConfigureDelegate { get; }
+    }
+}
