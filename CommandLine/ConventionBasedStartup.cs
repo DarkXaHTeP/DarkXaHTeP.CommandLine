0d147fce210b789a3e1b37865739a2b411f743b9
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-28 00:50:26 +0300

    Added convention based startup

---
+++ CommandLine/ConventionBasedStartup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandLine
+{
+    public class ConventionBasedStartup: IStartup
+    {
+        private readonly StartupMethods _methods;
+
+        public ConventionBasedStartup(StartupMethods methods)
+        {
+            _methods = methods;
+        }
+        
+        public void Configure(IApplicationBuilder app)
+        {
+            try
+            {
+                _methods.ConfigureDelegate(app);
+            }
+            catch (Exception ex)
+            {
+                if (ex is TargetInvocationException)
+                {
+                    ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+                }
+
+                throw;
+            }
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_methods.ConfigureServicesDelegate == null)
+            {
+                return;
+            }
+            
+            try
+            {
+                _methods.ConfigureServicesDelegate(services);
+            }
+            catch (Exception ex)
+            {
+                if (ex is TargetInvocationException)
+                {
+                    ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+                }
+
+                throw;
+            }
+        }
+    }
+}
