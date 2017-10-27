1fa04c4c375dfd7ace29e36442669cefe4241f9a
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-28 00:48:37 +0300

    Added configure and configure services builders

---
+++ CommandLine/ConfigureBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandLine
+{
+    public class ConfigureBuilder
+    {
+        public ConfigureBuilder(MethodInfo configure)
+        {
+            MethodInfo = configure;
+        }
+
+        public MethodInfo MethodInfo { get; }
+
+        public Action<IApplicationBuilder> Build(object instance) => builder => Invoke(instance, builder);
+
+        private void Invoke(object instance, IApplicationBuilder builder)
+        {
+            using (var scope = builder.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var parameterInfos = MethodInfo.GetParameters();
+                var parameters = new object[parameterInfos.Length];
+                for (var index = 0; index < parameterInfos.Length; index++)
+                {
+                    var parameterInfo = parameterInfos[index];
+                    if (parameterInfo.ParameterType == typeof(IApplicationBuilder))
+                    {
+                        parameters[index] = builder;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            parameters[index] = serviceProvider.GetRequiredService(parameterInfo.ParameterType);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception(string.Format(
+                                "Could not resolve a service of type '{0}' for the parameter '{1}' of method '{2}' on type '{3}'.",
+                                parameterInfo.ParameterType.FullName,
+                                parameterInfo.Name,
+                                MethodInfo.Name,
+                                MethodInfo.DeclaringType.FullName), ex);
+                        }
+                    }
+                }
+                MethodInfo.Invoke(instance, parameters);
+            }
+        }
+    }
+}
