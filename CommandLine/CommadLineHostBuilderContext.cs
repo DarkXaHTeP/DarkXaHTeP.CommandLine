08f6a4512d8cd7a5ca87cec1e7bd066a821e6685
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-27 03:24:46 +0300

    Added host builder context

---
+++ CommandLine/CommadLineHostBuilderContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CommandLine
+{
+    public class CommandLineHostBuilderContext
+    {
+        public ICommandLineEnvironment CommandLineEnvironment { get; set; }
+        public IConfiguration Configuration { get; set; }
+    }
+}
