de0d4442f7a5ada551d4e9c7d47c9c73247a1920
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-27 03:22:52 +0300

    Added IStartup interface

---
+++ CommandLine/IStartup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandLine
+{
+    public interface IStartup
+    {
+        void Configure(CommandLineApplication app);
+        void ConfigureServices(IServiceCollection services);
+    }
+}
