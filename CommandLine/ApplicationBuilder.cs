64faa1618094a13dd84d2653e4666bea608c68b1
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-28 02:42:20 +0300

    Added option, argument and command methods to ApplicationBuilder

--- CommandLine/ApplicationBuilder.cs
+++ CommandLine/ApplicationBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.CommandLineUtils;
 
 namespace CommandLine
@@ -16,5 +17,42 @@ namespace CommandLine
         
         public IServiceProvider ApplicationServices { get; }
         public IApplicationBuilder Parent { get; }
+
+        public CommandArgument Argument(string name, string description, bool multipleValues = false)
+        {
+            return _commandLineApp.Argument(name, description, multipleValues);
+        }
+
+        public IApplicationBuilder Command(
+            string name,
+            Action<IApplicationBuilder> configure,
+            bool throwOnUnexpectedArg = true)
+        {
+            Action<CommandLineApplication> configureAction = commandLineApp =>
+                configure(new ApplicationBuilder(ApplicationServices, commandLineApp, this));
+            var command = _commandLineApp.Command(name, configureAction, throwOnUnexpectedArg);
+
+            return new ApplicationBuilder(ApplicationServices, command, this);
+        }
+
+        public void OnExecute(Func<Task<int>> invoke)
+        {
+            _commandLineApp.OnExecute(invoke);
+        }
+
+        public void OnExecute(Func<int> invoke)
+        {
+            _commandLineApp.OnExecute(invoke);
+        }
+
+        public CommandOption Option(string template, string description, CommandOptionType optionType)
+        {
+            return _commandLineApp.Option(template, description, optionType);
+        }
+
+        public void ShowHelp(string commandName = null)
+        {
+            _commandLineApp.ShowHelp(commandName);
+        }
     }
 }
--- CommandLine/IApplicationBuilder.cs
+++ CommandLine/IApplicationBuilder.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace CommandLine
 {
     public interface IApplicationBuilder
     {
         IServiceProvider ApplicationServices { get; }
-        
+        IApplicationBuilder Parent { get; }
+        CommandArgument Argument(string name, string description, bool multipleValues = false);
+        IApplicationBuilder Command (string name, Action<IApplicationBuilder> configure, bool throwOnUnexpectedArg = true);
+        void OnExecute (Func<System.Threading.Tasks.Task<int>> invoke);
+        void OnExecute (Func<int> invoke);
+        CommandOption Option (string template, string description, CommandOptionType optionType);
+        void ShowHelp (string commandName = null);
     }
 }
