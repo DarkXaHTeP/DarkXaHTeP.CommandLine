5b64c5a60fdffdff1c5f5f26ab4eb50d3f9c15fa
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-15 02:03:26 +0200

    Added logging extensions to ICommandLineHostBuilder interface

--- CommandLine/ICommandLineHostBuilder.cs
+++ CommandLine/ICommandLineHostBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DarkXaHTeP.CommandLine
 {
@@ -10,6 +11,8 @@ namespace DarkXaHTeP.CommandLine
         ICommandLineHostBuilder ConfigureAppConfiguration(Action<CommandLineHostBuilderContext, IConfigurationBuilder> configureDelegate);
         ICommandLineHostBuilder ConfigureServices(Action<IServiceCollection> configureServices);
         ICommandLineHostBuilder ConfigureServices(Action<CommandLineHostBuilderContext, IServiceCollection> configureServices);
+        ICommandLineHostBuilder ConfigureLogging(Action<ILoggingBuilder> configureLogging);
+        ICommandLineHostBuilder ConfigureLogging(Action<CommandLineHostBuilderContext, ILoggingBuilder> configureLogging);
         ICommandLineHostBuilder UseStartup<TStartup>() where TStartup : class;
         ICommandLineHostBuilder AllowUnexpectedArgs();
         ICommandLineHost Build();
