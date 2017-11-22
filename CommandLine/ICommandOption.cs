06b80143cd8f96a270c79e1fe7c4f361f52706d3
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-22 02:44:02 +0200

    Added ICommandLineOption interface

---
+++ CommandLine/ICommandOption.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DarkXaHTeP.CommandLine
+{
+    public interface ICommandOption
+    {
+        string Template { get; set; }
+
+        string ShortName { get; set; }
+
+        string LongName { get; set; }
+
+        string SymbolName { get; set; }
+
+        string ValueName { get; set; }
+
+        string Description { get; set; }
+        
+        CommandLineOptionType OptionType { get; }
+        
+        List<string> Values { get; }
+        
+        bool HasValue();
+        
+        string Value();
+    }
+}
