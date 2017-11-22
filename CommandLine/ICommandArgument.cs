2f78d514759e4117d91378013349fe781e085520
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-22 02:53:28 +0200

    Added ICommandArgument interface

---
+++ CommandLine/ICommandArgument.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DarkXaHTeP.CommandLine
+{
+    public interface ICommandArgument
+    {
+        string Name { get; set; }
+        
+        string Description { get; set; }
+
+        List<string> Values { get; }
+
+        bool MultipleValues { get; set; }
+
+        string Value { get; }
+    }
+}
